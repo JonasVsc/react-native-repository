924f7ec3e71478a9ece9dfd765ae7447e18cd3e1
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2023-04-06 15:47:31 -0300

    navigation in progress..

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+//navigator
+import Routes from './routes/index.js'
+
+
+
+
+export default () => {
+  return (
+      <Routes/>
+  )
+}
